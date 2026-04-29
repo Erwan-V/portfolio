75a81a5569d4082c0b3521d0b9e300494478f49f
Erwan V <erwan.vasselin16@gmail.com>
2026-04-29 23:34:58 +0200

    add button dl CV

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,17 @@ export default function Hero() {
     <section className="hero">
       <h2>Erwan VASSELIN</h2>
       <p>étudiant en Bachelor cycle web et multimédia</p>
+      <div className="hero-buttons">
+
+        <a
+          href="/CV Erwan Vasselin.pdf"
+          download
+          className="btn"
+        >
+          Télécharger mon CV
+        </a>
+
+      </div>
     </section>
   );
 }
