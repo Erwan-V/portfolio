bc2ac9b0147fc42a689212eefdbfa9b37ebfef9e
Erwan V <erwan.vasselin16@gmail.com>
2026-04-29 23:19:45 +0200

    add image

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,12 +3,14 @@ export const projects = [
     title: "Premier projet",
     description:
       "Premier projet de développement web, réalisé en html, css. intégration d'une maquette, site vitrine.",
+    image: "/image-premier-projet.png",
     link: "https://premier-projet-rouge.vercel.app/",
   },
   {
     title: "première utilisation d'api",
     description:
       "site qui transforme un fichier csv en un graphique, réalisé en html, css, javascript.",
+    image: "/premier-api.png",
     link: "https://premiere-utilisation-api.vercel.app/",
   },
   {
